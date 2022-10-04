70cbacd552fb78a73eb3df38c78e7a4852afe4b1
Luis-HCarvalho <luishenriquecm12@gmail.com>
2022-10-04 04:43:17 -0300

    tentativa de alterações na organização de main.c, criação de funções para permitir maior versatilidade ao programa

--- main.c
+++ main.c
@@ -5,11 +5,27 @@
 
 #include "init.h"
 #include "combat.h"
+#include "map.h"
 
-// tamanho mapa (quantidade de tiles)
-#define mapWidth 60
-#define mapHeight 20
-#define sizeTile 32 // tamanho em px dos tiles
+// fazer uma func para draw player 
+// fazer uma func para draw monster
+// fazer uma func para draw maps
+
+void gameMainLoop (
+    ALLEGRO_TIMER * timer,
+    ALLEGRO_EVENT_QUEUE * queue,
+    ALLEGRO_FONT * font,
+    char map[][maxMapWidth],
+    Mapsize mapsize,
+    ALLEGRO_BITMAP * trollImg1,
+    ALLEGRO_BITMAP * trollImg2,
+    ALLEGRO_BITMAP * trollImg3,
+    ALLEGRO_BITMAP * trollImg4,
+    ALLEGRO_BITMAP * playerImg1,
+    ALLEGRO_BITMAP * playerImg2,
+    ALLEGRO_BITMAP * playerImg3,
+    ALLEGRO_BITMAP * playerImg4
+);
 
 int main () {
 
@@ -53,30 +69,7 @@ int main () {
     must_init(trollImg3, "trollImg3");
     must_init(trollImg4, "trollImg4");
 
-    // mapa
-    int map[mapHeight][mapWidth] = {
-        'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
-        'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
-        'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
-        'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-        't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-    };
-
+    // carrega tiles para o mapa
     ALLEGRO_BITMAP * tile1 = al_load_bitmap("./assets/tiles/tileTest.png");      // t
     ALLEGRO_BITMAP * tile2 = al_load_bitmap("./assets/tiles/tileTest2.png");     // b
     ALLEGRO_BITMAP * brickWall = al_load_bitmap("./assets/tiles/brickWall.png"); // w
@@ -90,7 +83,81 @@ int main () {
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
-    
+
+    // mapa
+    char map[maxMapHeight][maxMapWidth];    // matrix para armazenar o mapa
+    Mapsize mapsize;    // tamanho do mapa
+    mapsize = getMap("./maps/map1.txt", map, mapsize);  // pega o mapa e retorna o tamanho dele (h/w)
+
+    al_start_timer(timer);
+
+    // construção do mapa (coloca os tiles no lugar)
+    for (int i = 0; i < mapsize.height; i++) {
+        for (int j = 0; j < mapsize.width; j++) {
+            if (map[i][j] == 'w') {
+                al_draw_bitmap(brickWall, (j * sizeTile),  (i * sizeTile), 0);
+            }
+            else if (map[i][j] == 't') {
+                al_draw_bitmap(tile1, (j * sizeTile),  (i * sizeTile), 0);
+            }
+        }
+    }
+
+    gameMainLoop(
+        timer,
+        queue,
+        font,
+        map,
+        mapsize,
+        trollImg1,
+        trollImg2,
+        trollImg3,
+        trollImg4,
+        playerImg1,
+        playerImg2,
+        playerImg3,
+        playerImg4
+    );
+
+    // limpeza de recursos criados durante as inicializações
+    al_destroy_font(font);
+    al_destroy_display(display);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    al_destroy_bitmap(playerImg1);
+    al_destroy_bitmap(playerImg2);
+    al_destroy_bitmap(playerImg3);
+    al_destroy_bitmap(playerImg4);
+
+    al_destroy_bitmap(trollImg1);
+    al_destroy_bitmap(trollImg2);
+    al_destroy_bitmap(trollImg3);
+    al_destroy_bitmap(trollImg4);
+
+    return 0;
+}
+
+// loop principal de execução do programa
+void gameMainLoop (
+    ALLEGRO_TIMER * timer,
+    ALLEGRO_EVENT_QUEUE * queue,
+    ALLEGRO_FONT * font,
+    char map[][maxMapWidth],
+    Mapsize mapsize,
+    ALLEGRO_BITMAP * trollImg1,
+    ALLEGRO_BITMAP * trollImg2,
+    ALLEGRO_BITMAP * trollImg3,
+    ALLEGRO_BITMAP * trollImg4,
+    ALLEGRO_BITMAP * playerImg1,
+    ALLEGRO_BITMAP * playerImg2,
+    ALLEGRO_BITMAP * playerImg3,
+    ALLEGRO_BITMAP * playerImg4
+) {
+
+    ALLEGRO_EVENT event;
+    //al_start_timer(timer);
+
     // player
     Player player;
     player = initPlayer(player);
@@ -105,11 +172,13 @@ int main () {
     bool combat = false;
     bool attack = false;
 
-    ALLEGRO_EVENT event;
-    al_start_timer(timer);
+    // monstros >> colocar uma funcçao de criação de monstro 
 
-    // loop principal de execução do programa
     while (!done) {
+
+        // limpa a tela
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+
         al_wait_for_event(queue, &event); // aguarda evento acontecer para que o programa continuar
 
         switch(event.type) {
@@ -194,22 +263,6 @@ int main () {
             break;
 
         if(redraw && al_is_event_queue_empty(queue)) {
-            // limpa a tela
-            al_clear_to_color(al_map_rgb(0, 0, 0));
-
-
-            // construção do mapa (coloca os tiles no lugar)
-            for (int i = 0; i < mapHeight; i++) {
-                for (int j = 0; j < mapWidth; j++) {
-                    if (map[i][j] == 'w') {
-                        al_draw_bitmap(brickWall, (j * sizeTile),  (i * sizeTile), 0);
-                    }
-                    else if (map[i][j] == 't') {
-                        al_draw_bitmap(tile1, (j * sizeTile),  (i * sizeTile), 0);
-                    }
-                }
-            }
-
             // desenha a sprite do player
             if (player.playerAnim < 10) {
                 al_draw_bitmap(playerImg1, player.x, player.y, player.direc);
@@ -267,21 +320,4 @@ int main () {
             redraw = false;
         }
     }
-
-
-    // limpeza de recursos criados durante as inicializações
-    al_destroy_font(font);
-    al_destroy_display(display);
-    al_destroy_timer(timer);
-    al_destroy_event_queue(queue);
-    al_destroy_bitmap(playerImg1);
-    al_destroy_bitmap(playerImg2);
-    al_destroy_bitmap(playerImg3);
-    al_destroy_bitmap(playerImg4);
-    al_destroy_bitmap(trollImg1);
-    al_destroy_bitmap(trollImg2);
-    al_destroy_bitmap(trollImg3);
-    al_destroy_bitmap(trollImg4);
-
-    return 0;
 }
