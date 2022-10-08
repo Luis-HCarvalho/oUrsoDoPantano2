1f45bb1bc8a00b0d5e4b25fd8231751cb7bd8ae7
Luis-HCarvalho <luishenriquecm12@gmail.com>
2022-10-08 20:11:40 -0300

    add enum para mapas

--- headers/map.h
+++ headers/map.h
@@ -13,6 +13,14 @@ typedef struct {
     int wall;
 } Mapsize;
 
+// substituir posteriormente pelo nome dos mapas
+enum MapNavigation {
+    room0 = 0,
+    room1,
+    room2,
+    room3,
+};
+
 // recebe o path para o txt do mapa e uma matrix para armazenar map[maxMapHeight][maxMapWidth]
 void getMap (char mapName[], char map[][maxMapWidth], Mapsize * mapsize);
 
